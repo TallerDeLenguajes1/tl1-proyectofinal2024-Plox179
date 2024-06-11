e41add21c60026cc5fc7df50bdf6fe40ab5ba585
Plox179 <leandroleon799@gmail.com>
2024-06-11 16:26:21 -0300

    Creo la clase Personaje y FabricaDePersonaje

---
+++ FabricaDePersonaje.cs
@@ -0,0 +1,27 @@
+namespace FabricaDePersonaje;
+
+using System.IO.Pipes;
+using Microsoft.VisualBasic;
+using Personaje;
+
+public class FabricaDePersonaje
+{
+    public Personaje CrearPersonaje()
+    {
+        Random numRand = new Random();
+        int dia = numRand.Next(1,32);
+        int mes = numRand.Next(1,13);
+        int anio = numRand.Next(1800, 2025);
+        Personaje miPersonaje = new Personaje();
+        miPersonaje.Nombre = "...";
+        miPersonaje.Apodo = "asd";
+        miPersonaje.Armadura = numRand.Next(1,11);
+        miPersonaje.Edad = numRand.Next(1,301);
+        miPersonaje.FechaNac = new DateTime(dia,mes,anio);
+        miPersonaje.Nivel = numRand.Next(1,11);
+        miPersonaje.Tipo = "Guerrero";
+        miPersonaje.Velocidad = numRand.Next(1,11);
+        miPersonaje.Fuerza = numRand.Next(1,11);
+        return miPersonaje;
+    }
+}
---
+++ Personaje.cs
@@ -0,0 +1,28 @@
+namespace Personaje;
+
+public class Personaje
+{
+    string tipo;
+    string nombre;
+    string apodo;
+    DateTime fechaNac;
+    int edad;
+    int velocidad;
+    int destreza;
+    int fuerza;
+    int nivel;
+    int armadura;
+    int salud = 100;
+
+    public string Tipo { get => tipo; set => tipo = value; }
+    public string Nombre { get => nombre; set => nombre = value; }
+    public string Apodo { get => apodo; set => apodo = value; }
+    public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+    public int Edad { get => edad; set => edad = value; }
+    public int Velocidad { get => velocidad; set => velocidad = value; }
+    public int Fuerza { get => fuerza; set => fuerza = value; }
+    public int Nivel { get => nivel; set => nivel = value; }
+    public int Armadura { get => armadura; set => armadura = value; }
+    public int Salud { get => salud; set => salud = value; }
+}
+
